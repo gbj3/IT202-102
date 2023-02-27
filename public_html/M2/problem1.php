edacbd7b9ebbe236e1e7b0b7e577b40f8c92ef19
gbj3 <gbj3@njit.edu>
2023-02-26 19:17:25 -0500

    completed 1

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -6,6 +6,12 @@ $a4 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
+    for ($i=0; $i<count($arr); $i++) {
+        if(($arr[$i]%2 != 0)) {
+            echo $arr[$i];
+        }
+
+    }
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
 
