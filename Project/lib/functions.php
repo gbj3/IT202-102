e5b026320d5412dced396749c664da4d29230ce6
gbj3 <gbj3@njit.edu>
2023-03-29 21:55:25 -0400

    base

---
+++ Project/lib/functions.php
@@ -0,0 +1,11 @@
+<?php
+//TODO 1: require db.php
+
+//require safer_echo.php
+require(__DIR__ . "/safer_echo.php");
+//TODO 2: filter helpers
+
+//TODO 3: User helpers
+
+//TODO 4: Flash Message Helpers
+?>
